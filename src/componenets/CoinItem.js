1fd0611f09478f19ffebf2b485477142f58d2c40
Atharva Mulgund <atharva.mulgund@gmail.com>
2022-06-22 23:57:38 +0530

    Final Build

--- src/componenets/CoinItem.js
+++ src/componenets/CoinItem.js
@@ -10,7 +10,7 @@ const CoinItem = (props) => {
                 <p>{props.coins.symbol.toUpperCase()}</p>
             </div>
             <p>${props.coins.current_price.toLocaleString()}</p>
-            <p>{props.coins.price_change_percentage_24h.toFixed(2)}%</p>
+            <p >{props.coins.price_change_percentage_24h.toFixed(2)}%</p>
             <p className='hide'>${props.coins.total_volume.toLocaleString()}</p>
             <p className='hide'>${props.coins.market_cap.toLocaleString()}</p>
         </div>
