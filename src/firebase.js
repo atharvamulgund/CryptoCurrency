d051c199869fe927c6b410ac70c8eec71128aaad
Atharva Mulgund <atharva.mulgund@gmail.com>
2022-06-23 00:36:30 +0530

    Firebase Added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAe3BQhu3quMFbF7YVZx5tY-tk7zAhvnXg",
+  authDomain: "crypto-tracker-e9a0c.firebaseapp.com",
+  projectId: "crypto-tracker-e9a0c",
+  storageBucket: "crypto-tracker-e9a0c.appspot.com",
+  messagingSenderId: "946773814646",
+  appId: "1:946773814646:web:5663d6054080da321b5e57"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
